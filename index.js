4262f0bd332144fb6845067ff38ddaeee1189e73
manmaul <proyectosmanmaul@gmail.com>
2025-11-22 13:22:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from "express";
+import { getTikTokTop30 } from "./src/tiktok.js";
 
 const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.get("/", (req, res) => {
-  res.send("Funny Scraper Running OK ✔");
+  res.send("Funny Scraper Running ✔");
+});
+
+app.get("/scrape/tiktok", async (req, res) => {
+  try {
+    const data = await getTikTokTop30();
+    res.json({ ok: true, total: data.length, videos: data });
+  } catch (err) {
+    res.json({ ok: false, error: err.message });
+  }
 });
 
 app.listen(PORT, () => {

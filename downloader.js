0bd0c895d7a5195df7d3f3a39dc4b823576acdd0
manmaul <proyectosmanmaul@gmail.com>
2025-11-23 16:43:02 +0100

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -17,8 +17,8 @@ function runYtDlpCommand(url, outputPath, videoId) {
       '--restrict-filenames',
       // Argumento para seleccionar el mejor stream (mp4 o m4a) y una altura máxima de 1080p
       '-S', 'ext:mp4:m4a,height:1080',
-      // Eliminamos el argumento --max-duration 59 para evitar el error 'no such option'
-      // '--max-duration', '59', 
+      // ARGUMENTO REINTRODUCIDO: Limita la descarga a 59 segundos
+      '--max-duration', '59', 
       // Este argumento asegura que yt-dlp funcione con la URL estable de Big Buck Bunny
       '--extractor-args', 'youtube:player_client=default', 
       url

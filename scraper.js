adac2c8ee0c333fc4196cd3aad0cdf1d4cfb5fb7
manmaul <proyectosmanmaul@gmail.com>
2025-11-23 15:08:52 +0100

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -11,7 +11,7 @@ const REELS_COUNT = 5;
 const OUT_FILE = path.join(process.cwd(), 'data', 'video_list.json');
 
 // ID de un video de YouTube de muestra que SÍ está disponible (Nuevo ID)
-const SAMPLE_YOUTUBE_ID = 'kXoDkY0gU0U';
+const SAMPLE_YOUTUBE_ID = 'vV-13_R6Q1k';
 
 const VideoScraper = {
   // --- Simulación de Scraping ---
